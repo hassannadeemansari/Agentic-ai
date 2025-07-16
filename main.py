4304a8303b51c3bc8d67253dd923302893d7507c
Hassan Nadeem <164927940+hassannadeemansari@users.noreply.github.com>
2025-07-16 23:21:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import os
+from dotenv import load_dotenv
+
 from agents import (
    Agent ,  #a
    Runner ,  
    OpenAIChatCompletionsModel ,
    RunConfig
 )
+
+load_dotenv()
+
+First_agent = Agent(name = "Helloo-agent" , instruction = "you are a helpfull agent ")
